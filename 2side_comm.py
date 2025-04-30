ae91612c1167e3fcd219180429c6e9b2d4091e9f
Kristian Tasho <kristiantasho12@gmail.com>
2025-04-30 10:35:24 +0200

    AFEO

--- 2side_comm.py
+++ 2side_comm.py
@@ -5,13 +5,23 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
 
-    while True:
-        # Send message to Arduino
-        ser.write(b"Hello from Raspberry Pi!\n")
+    value_to_send = 1
 
-        # Check for response
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(f"Raspberry Pi received: {line}")
+    while True:
+        # Send integer to Arduino
+        message = f"{value_to_send}\n"
+        ser.write(message.encode('utf-8'))
+
+        # Read response(s) from Arduino
+        responses = []
+        start_time = time.time()
+        while time.time() - start_time < 1:  # Wait up to 1 second
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+                if line:
+                    responses.append(line)
+
+        for response in responses:
+            print(f"Raspberry Pi received: {response}")
 
         time.sleep(1)
